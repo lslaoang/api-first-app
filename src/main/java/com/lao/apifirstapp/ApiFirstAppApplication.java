8bc449e1a53ea4d8ecd2d88d705d2c4b25a8a3ca
Laurel Lao Ang <laurellaoang@gmail.com>
2022-01-17 22:21:20 +0800

    Initial commit

---
+++ src/main/java/com/lao/apifirstapp/ApiFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.lao.apifirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFirstAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lao/apifirstapp/ApiFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.apifirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
