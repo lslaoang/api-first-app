d6f6c9b1a5309db0a6cc4173be9a8862ba65f6ce
Laurel Lao Ang <laurellaoang@gmail.com>
2022-01-26 19:00:27 +0800

    removed unused controllers

--- src/main/java/com/lao/apifirstapp/controller/UserController.java
+++ src/main/java/com/lao/apifirstapp/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.lao.apifirstapp.controller;
 
 
-
 import com.lao.apifirstapp.model.Response;
 import com.lslao.af.api.UsersApi;
 import com.lslao.af.models.ResponseSchema;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -38,17 +35,6 @@ public class UserController implements UsersApi {
         }
 
         return new ResponseEntity<>(response,HttpStatus.OK);
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<?> testResponse(){
-        return new ResponseEntity<>(HttpStatus.ACCEPTED);
-    }
-
-    @PostMapping(value ="/checkraw")
-    public ResponseEntity<String>  getPostResp() {
-
-        return ResponseEntity.ok("test");
     }
 
 }
